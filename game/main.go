57eca1656011d12dcd8464c3a4545e28181ba8ee
iakud <kaikai87111@gmail.com>
2021-03-30 16:46:59 +0800

    game app

--- game/main.go
+++ game/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/iakud/plume"
+)
 
+type GameApp struct {
+}
+
+func (game *GameApp) Init() {
+
+}
+
+func (game *GameApp) Destory() {
+
+}
+
+func main() {
+	plume.Run(&GameApp{})
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/iakud/plumeserver
+
+require github.com/iakud/plume v0.0.0-20210330081517-b33608706bf7
